f342e31b70a57dc8685b6a273141370035f05e96
tduchnowski <entidzow@gmail.com>
2021-07-08 04:09:56 +0200

    Add sleep function and clear screen function to bootloader

--- boot/io.asm
+++ boot/io.asm
@@ -2,17 +2,30 @@
 ; Params:
 ; 	cx = number of characters in a string 
 ; 	bp = pointer to a string
+; 	dh = row
+; 	dl = column
 print_string:
 	mov al, 1
 	mov bh, 0
 	mov bl, 0eh ; color
-	mov dl, 30 ; row
-	mov dh, 15 ; column
-	;mov cx, msglen
 	push cs
 	pop es
-	;mov bp, msg 
 	mov ah, 13h
 	int 10h
 	ret
 	
+; Clear the whole screen
+clear_screen:
+	mov ah, 0
+	mov al, 0x03
+	int 10h
+	ret
+
+; Sleep
+; Params:
+; 	CX:DX = interval in miliseconds
+sleep:
+	mov al, 0
+	mov ah, 86h
+	int 15h
+	ret
